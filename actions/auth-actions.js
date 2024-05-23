6f23620f84a9d3df7a89bc3feb55611011777d30
heliosalesjr <heliosalesjr@gmail.com>
2024-05-23 14:59:07 -0300

    actions folder + .env test

---
+++ actions/auth-actions.js
@@ -0,0 +1,23 @@
+'use server';
+
+export async function signup(prevState, formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    let errors = {};
+
+    if (!email.includes('@')) {
+        errors.email = 'Enter a valid email address.';
+    }
+
+    if (password.trim().length < 8) {
+        errors.password = 'Enter a valid password, we need at least 8 characters.';
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return {
+            errors,
+        }
+    }
+
+}

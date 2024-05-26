1c99f30a08e42d0a733db97d1562ee0ae271f7de
heliosalesjr <heliosalesjr@gmail.com>
2024-05-26 17:52:17 -0300

    handling duplicated emails

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,29 +1,42 @@
 'use server';
+import { redirect } from 'next/navigation';
 
 import { hashUserPassword } from '@/lib/hash';
 import { createUser } from '@/lib/user';
 
 export async function signup(prevState, formData) {
-    const email = formData.get('email');
-    const password = formData.get('password');
+  const email = formData.get('email');
+  const password = formData.get('password');
 
-    let errors = {};
+  let errors = {};
 
-    if (!email.includes('@')) {
-        errors.email = 'Enter a valid email address.';
-    }
+  if (!email.includes('@')) {
+    errors.email = 'Please enter a valid email address.';
+  }
 
-    if (password.trim().length < 8) {
-        errors.password = 'Enter a valid password, we need at least 8 characters.';
-    }
+  if (password.trim().length < 8) {
+    errors.password = 'Password must be at least 8 characters long.';
+  }
 
-    if (Object.keys(errors).length > 0) {
-        return {
-            errors,
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  const hashedPassword = hashUserPassword(password);
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+      return {
+        errors: {
+          email: 'It seems like an account for the chosen email already exists.'
         }
+      };
     }
-    const hashedPassword = hashUserPassword(password);
+    throw error;
+  }
 
-    createUser(email, password)
-
+  redirect('/training');
 }
